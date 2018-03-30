188a6729b99be1044e4d155472bac347aded763c
nikoloza <nika.tomadze@gmail.com>
2018-03-30 08:48:23 +0400

    switching slides restructure

--- assets/main.js
+++ assets/main.js
@@ -1,57 +1,150 @@
-// carousel scripting
-(function(){
-  let main = document.querySelector('main')
-  let figures = document.querySelectorAll('figure')
-  let lastProjectIndex = figures.length - 1
+// helpers
+const compose = (...fns) =>
+  fns.reverse().reduce((prevFn, nextFn) =>
+    value => nextFn(prevFn(value)),
+    value => value
+  )
 
-  // activate first item
-  var activeProject = 0
+;(function(){
+  const SETTINGS = {
+    rootNode: document.querySelector('.slider'),
+    slideNodes: document.querySelectorAll('.slide'),
+    arrowNodes: document.querySelectorAll('.arrows svg'),
+
+    // slider settings
+    activeIndex: 0,
+    slidingDuration: 750,
+  }
+
+  SETTINGS.lastIndex = SETTINGS.slideNodes.length - 1
+
+  /**
+   * Initialize website animations 
+   * and activate first slide
+   */
+  window.onload = compose(
+    initial.bind(SETTINGS),
+    slideSwitcher.bind(SETTINGS, undefined, initial),
+    arrowsClickHandler.bind(SETTINGS),
+    arrowsKeyHandler.bind(SETTINGS),
+  )
+
+  /**
+   * Initialize website content
+   * and remove loading effect
+   */
+  function initial () {
+    let { rootNode } = this
+    let { classList } = rootNode
+    classList.add('ready')
+    classList.remove('loading')
+  }
   
-  var t = window.setTimeout(() => 0)
-  function activateProject (n) {
-    main.classList.remove('loading')
-    window.clearTimeout(t)
+  /**
+   * Switching a slide
+   */
+  function slideSwitcher (index) {
+    let { 
+      rootNode, 
+      slideNodes,
+      activeIndex,
+      lastIndex, 
+      slidingDuration 
+    } = this
     
-    if (n < 0) n = lastProjectIndex
-    else if (n > lastProjectIndex) n = 0
-    main.classList.add('loading')
-    figures[activeProject].classList.add('out')
+    console.log(activeIndex, index)
     
-    t = window.setTimeout(() => {
-      let previous = figures[activeProject]
-      previous.classList.remove('active', 'out')
+    // Stop if asking for the same slide
+    if (index && index === activeIndex) return
 
-      let active = figures[n]
-      active.classList.add('active')
+    // Define updated information
+    let newActiveIndex = index ? repeatIndex.call(this, index) : 0
+    let direction = activeIndex < newActiveIndex ? 'left' : 'right'
 
-      // update active page
-      activeProject = n
+    // Define previous and next nodes
+    let previousNode = slideNodes[activeIndex]
+    let nextNode = slideNodes[newActiveIndex]
 
-      main.classList.add('ready')
+    // Animation state 1: 
+    if (previousNode !== nextNode) 
+      animateOut(previousNode, direction, slidingDuration)
+    
+    // Animation state 2: 
+    let timeout = window.setTimeout(() => {
+      animateIn(nextNode, direction, slidingDuration)
+      window.clearTimeout(timeout)
+    }, slidingDuration)
 
-      t = window.setTimeout(() => {
-        main.classList.remove('loading')
-        window.clearTimeout(t)
-      }, 750)
-    }, 750)
+    // Update active slide index in Settings
+    this.activeIndex = newActiveIndex
   }
 
-  window.onload = () => {
-    activateProject(activeProject)
+  /**
+   * Check if new index is below, or above the range of slides 
+   */
+  function repeatIndex (index) {
+    let { lastIndex } = this
+    // If below the first slide, continue from the last
+    if (index < 0) return lastIndex
+    // If above of its total amount, continue from the first
+    else if (index > lastIndex) return 0
+    return index
   }
 
-  // arrows clicking
-  let arrows = document.querySelectorAll('.arrows svg')
-  let leftArrow = arrows[0]
-  let rightArrow = arrows[1]
+  
+  function animateOut (previousNode, direction, slidingDuration) {
+    previousNode.classList.add('out', direction)
 
-  leftArrow.addEventListener('click', () => activateProject(activeProject - 1))
-  rightArrow.addEventListener('click', () => activateProject(activeProject + 1))
+    let timeout = window.setTimeout(() => {
+      previousNode.classList.remove('active', 'out', 'in', 'left', 'right')
+      window.clearTimeout(timeout)
+    }, slidingDuration)
+  }
 
-  document.addEventListener('keydown', (e) => {
-    let { keyCode } = e
-    let direction = keyCode - 38
-    if (keyCode === 37 || keyCode === 39)
-      activateProject(activeProject + direction)
-  })
+  function animateIn (nextNode, direction, slidingDuration) {
+    nextNode.classList.add('active', 'in', direction)
+    
+    let timeout = window.setTimeout(() => {
+      nextNode.classList.remove('in', direction)
+      window.clearTimeout(timeout)
+    }, slidingDuration)
+  }
+
+  /** 
+   * Arrows Triggering
+   * on pressing arrow icons
+   */
+  function arrowsClickHandler () {
+    let that = this
+
+    // Define arrow DOM node variables
+    let leftArrow = this.arrowNodes[0]
+    let rightArrow = this.arrowNodes[1]
+
+    // Add event listeners
+    leftArrow.addEventListener('click', () => 
+      slideSwitcher.call(that, that.activeIndex - 1))
+    rightArrow.addEventListener('click', () => 
+      slideSwitcher.call(that, that.activeIndex + 1))
+  }
+
+  /** 
+   * Arrows Triggering on keyboard arrows
+   * to switch the slide
+   */
+  function arrowsKeyHandler () {
+    let that = this
+
+    document.addEventListener('keydown', (e) => {
+      let { activeIndex } = that
+      let { keyCode } = e
+
+      // Determine direction (it's -1 for the left and +1 for right)
+      let direction = keyCode - 38
+      
+      // Run the function only on arrow keys
+      if (keyCode === 37 || keyCode === 39)
+        slideSwitcher.call(that, activeIndex + direction)
+    })
+  } 
 })()
