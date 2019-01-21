27317a1545450397d8a68bf38d3d84b4d9e84552
nikoloza <nikoloza@Nikas-iMac.local>
2019-01-21 06:16:12 +0400

    project updates

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,7 @@ const createArray = (length) =>
     rootNode: document.querySelector('.slider'),
     slideNodes: document.querySelectorAll('.slide'),
     menuItemNodes: document.querySelectorAll('.menu a'),
-    arrowNodes: document.querySelectorAll('.arrows svg'),
+    arrowNodes: document.querySelectorAll('.arrows i'),
 
     // slider settings
     activeIndex: 0,
@@ -69,14 +69,14 @@ const createArray = (length) =>
     if (index && index === activeIndex) return
 
     // Define updated information
-    let newActiveIndex = index ? repeatIndex.call(this, index) : 0
-    console.log(direction)
+    let newActiveIndex = repeatIndex.call(this, index || 0)
     updateDirection.call(this, direction)
 
     // Define previous and next nodes
     let previousNode = slideNodes[activeIndex]
     let nextNode = slideNodes[newActiveIndex]
 
+    // Switching to a project
     let isProject = 
       previousNode.classList.contains('project') || 
       nextNode.classList.contains('project')
@@ -99,6 +99,9 @@ const createArray = (length) =>
     // Update active slide index in Settings
     this.activeNode = nextNode
     this.activeIndex = newActiveIndex
+
+    // Activate menu item
+    activateNavigation.call(this)
   }
 
   /**
@@ -106,10 +109,25 @@ const createArray = (length) =>
    */
   function repeatIndex (index) {
     let { lastIndex } = this
+    let leftArrow = this.arrowNodes[0]
+    let rightArrow = this.arrowNodes[1]
+
+    console.log(index, lastIndex)
+    
     // If below the first slide, continue from the last
-    if (index < 0) return lastIndex
+    if (index <= 0) {
+      leftArrow.classList.add('disabled')
+      rightArrow.classList.remove('disabled')
+      return 0
+    }
     // If above of its total amount, continue from the first
-    else if (index > lastIndex) return 0
+    else if (index >= lastIndex) {
+      rightArrow.classList.add('disabled')
+      leftArrow.classList.remove('disabled')
+      return lastIndex
+    }
+    leftArrow.classList.remove('disabled')
+    rightArrow.classList.remove('disabled')
     return index
   }
 
@@ -157,8 +175,27 @@ const createArray = (length) =>
       let index = parseInt(item.dataset.navigationIndex)
       if (index < 0) index = slidesLength + index
       item.addEventListener('click', (e) => {
-        slideSwitcher.call(that, index)
+        slideSwitcher.call(that, index, index > that.activeIndex ? 1 : -1)
       })
+    })
+  }
+
+  function activateNavigation () {
+    let { menuItemNodes, activeIndex, slidesLength } = this
+
+    // run on nodes as array items
+    let itemNodesArr = createArray(menuItemNodes.length)
+    itemNodesArr.map((v, key) => {
+      let item = menuItemNodes[key]
+      let index = parseInt(item.dataset.navigationIndex)
+      if (index < 0) index = slidesLength + index
+      if (activeIndex >= index) {
+        item.classList.add('active')
+        itemNodesArr.filter((c, key) => {
+          let sibling = menuItemNodes[key]
+          if (sibling !== item) sibling.classList.remove('active')
+        })
+      }
     })
   }
 
